d0c607594902ec2a55779070a512bd6424fab2e6
shin0624 <91828379+shin0624@users.noreply.github.com>
2023-02-04 22:41:09 +0900

    Add files via upload

---
+++ 자동차추상화.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Car
+{
+    public string Name = null;
+    public int Vel = 0;
+    public string Gear = null;
+
+    public Car(string name, string gear) {
+
+        Name = name;
+        Gear = gear;
+       
+
+        Console.WriteLine("자동차의 이름은 " + Name + "입니다.\n");
+        Console.WriteLine("자동차가 " + Gear + "합니다.\n");
+        
+        
+    }
+
+    public  Car(int vel, string gear)
+    {
+        Vel = vel;
+        Gear = gear;
+        Console.WriteLine("자동차의 속도는 시속" + Vel + "입니다.\n");
+        Console.WriteLine("자동차가 " + Gear + "합니다.\n");
+
+    }
+    
+    }
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Car sonata = new Car("소나타","전진");
+        Car velgear = new Car(80, "후진");
+
+        Console.WriteLine("자동차가 멈춥니다.\n");
+    }
+}
+
