24f61c904cd1ffdab1ea9ef99312dccd2f4b7a4b
shin0624 <91828379+shin0624@users.noreply.github.com>
2023-02-08 22:16:28 +0900

    Add files via upload

---
+++ 회문검증.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        bool result = true;
+        Console.WriteLine("단어를 영문으로 입력하세요 : ");
+        string word = Console.ReadLine();
+         
+        Queue qu = new Queue();
+        Stack st = new Stack();
+
+        foreach (char item in word)
+        {
+            st.Push(item);
+            qu.Enqueue(item);
+        }
+
+
+
+        while (qu.Count > 0)
+        {
+            char A = (char)qu.Dequeue();
+            char B = (char)st.Pop();
+
+            if (A != B)
+            {
+                result = false;
+                break;
+            }
+
+        }
+        if (result)
+            Console.WriteLine("참(True)");
+        else
+            Console.WriteLine("거짓(False)");
+       
+    }
+    
+}
