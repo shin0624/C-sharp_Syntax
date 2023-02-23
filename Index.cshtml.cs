8ac77ef44c949bc678509f6734e2d3ce6d4e9f15
shin0624 <91828379+shin0624@users.noreply.github.com>
2023-02-23 22:25:10 +0900

    Update Index.cshtml.cs

--- Index.cshtml.cs
+++ Index.cshtml.cs
@@ -1,19 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace MyWebApp2.Pages;
-
-public class IndexModel : PageModel
-{
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
-
-    public void OnGet()
-    {
-
-    }
+@page
+@model IndexModel
+@{
+    ViewData["Title"] = "Home page";
 }
+
+<div class="text-center">
+    <h1 class="display-4">Hello World!</h1>
+    <p>오늘은 [@DateTime.Now.DayOfWeek]입니다.</p>
+   <p>현재 시간은[ @DateTime.Now]입니다.</p>
+</div>
