42a8f98e7cc1dd42f1acc0f15a4347ddfdb319d8
bbs <91828379+shin0624@users.noreply.github.com>
2023-06-28 21:00:22 +0900

    일반화 메서드로 배열의 값 출력하기
    C# 일반화 기본형
    접근제한자 반환형 메서드 이름 <T>(T[] 매개변수)
    {
    }

---
+++ Generic_Method.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+/*class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int[] intValues = { 1, 2, 3 };
+        double[] doubleValues = { 1.1, 2.2, 3.3 };
+
+        PrintValue<int>(intValues);
+        PrintValue<double>(doubleValues);
+
+    }
+    static void PrintValue<T>(T[] values)
+    {
+        foreach(var i in values)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}*/
+
+class GenericClass<T>
+{
+    private T[] values;
+    private int count;
+
+    public GenericClass(int length)
+    {
+        values = new T[length];
+        count = length;
+    }
+
+    public void SetValue (T item, int index)
+    {
+        this.values[index] = item;
+    }
+
+    public void PrintValue()
+    {
+        foreach(T item in values)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        GenericClass<int> intArray = new GenericClass<int>(3);
+        GenericClass<double> doubleArray = new GenericClass<double>(3);
+
+        intArray.SetValue(1, 0);
+        intArray.SetValue(2, 1);
+        intArray.SetValue(3, 2);
+        intArray.PrintValue();
+
+        doubleArray.SetValue(1.1, 0);
+        doubleArray.SetValue(2.2, 1);
+        doubleArray.SetValue(3.3, 2);
+        doubleArray.PrintValue();
+    }
+}
