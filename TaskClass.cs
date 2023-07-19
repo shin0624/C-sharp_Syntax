a02a66cb9c896a1a1fc2200767dacd22fa8998b2
bbs <91828379+shin0624@users.noreply.github.com>
2023-07-19 23:54:20 +0900

    Task 클래스로 비동기 시점 제어
    비동기 시점 제어 클래스인 Task클래스 사용, await 사용으로 작업 순서의 임의 변경이 가능

---
+++ TaskClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class MainClass
+{
+    public static async Task Main(string[] args)
+    {
+        Task t = AsyncFunc();
+        Console.WriteLine("1");
+        await t;
+        Console.WriteLine("2");
+    }
+
+    public static async Task AsyncFunc()
+    {
+        await Task.Delay(2000);
+        Console.WriteLine("3");
+    }
+}
